a2cdc61b232200733d210406d638c36d10e07b00
Mirian Barajas <mirianbarcam@gmail.com>
2022-07-06 20:50:14 +0000

    completed exercise

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".us", ".io"];
+
+  let list = [];
+
+  function generateString() {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < adj.length; n++) {
+          for (let e = 0; e < ext.length; e++) {
+            list.push(pronoun[i] + adj[a] + noun[n] + ext[e]);
+          }
+        }
+      }
+    }
+  }
+
+  generateString();
+
+  for (let e = 0; e < list.length; e++) {
+    let li = document.createElement("li");
+    let text = document.createTextNode(list[e]);
+    li.appendChild(text);
+    document.getElementById("listado").appendChild(li);
+  }
 };
